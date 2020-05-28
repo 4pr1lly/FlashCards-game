662fcc9a0cf99d55a6fb05cd3637878269dbb49d
April Murphy <amurphy24@humana.com>
2020-05-28 17:15:50 -0400

    2nd edit 05/28

--- flashCards.js
+++ flashCards.js
@@ -1,29 +1,62 @@
 
 let userFlippedCard= false;
-let firstCard, secondCard
+let firstCard, secondCard;
+let lockBoard = false; 
 
 const cards = document.querySelectorAll('.cards');
 
 function flipCard() {
     /*console.log("I was flipped");*/
+if (lockBoard) return;  
+if(this===firstCard) return; // function to stop doubclick of the card and holding on the board
 this.classList.add("flip");
+
+
 if (!userFlippedCard) { //when the user clicks the card for the first time.
     userFlippedCard = true;
     firstCard =this;
-}else {
+  return;  
+}
     // second card flip
     userFlippedCard= false;
     secondCard = this;
 
 //checking if the cards match
 
-if (firstCard.dataset.framework === secondCard.dataset.framework)
+    checkForMatch (); 
+    }
+
+  function checkForMatch () {
+    let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;  //using Ternary operator
+        
+        isMatch ? disableCards() : unflipCards(); // ternary allows if else statement in just one line
+
+     }
 
 
-    
-}
+    function disableCards(){
+        firstCard. removeEventListener('click', flipCard);
+        secondCard. removeEventListener('click', flipCard);
 
-}
+        resetBoard();
+    }
+
+    function unflipCards (){
+        lockBoard = true;// unlocks cards after they are flipped
+
+        setTimeout(() => {
+            firstCard.classList.remove ("flip");
+            secondCard.classList.remove ("flip");
+
+            resetBoard();
+            }, 1200);
+         }
+     function resetBoard() {
+
+       [userFlippedCard, lockBoard] = [false, false];
+       [firstCard. secondCard] = [null, null];
+    }   
+     
 
 cards.forEach(card=> card.addEventListener ("click", flipCard));
 
