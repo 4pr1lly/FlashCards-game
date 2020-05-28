18cb5fcaf4a7d4c607480ec7dc987a4850695e26
April Murphy <amurphy24@humana.com>
2020-05-28 14:13:53 -0400

    1st edit 05/28

--- flashCards.js
+++ flashCards.js
@@ -1,9 +1,27 @@
 
+let userFlippedCard= false;
+let firstCard, secondCard
+
 const cards = document.querySelectorAll('.cards');
 
 function flipCard() {
-    /*console.log("I was clicked");*/
-this.classList.toggle("flip");
+    /*console.log("I was flipped");*/
+this.classList.add("flip");
+if (!userFlippedCard) { //when the user clicks the card for the first time.
+    userFlippedCard = true;
+    firstCard =this;
+}else {
+    // second card flip
+    userFlippedCard= false;
+    secondCard = this;
+
+//checking if the cards match
+
+if (firstCard.dataset.framework === secondCard.dataset.framework)
+
+
+    
+}
 
 }
 
